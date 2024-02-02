56e63ab8f40f319d94bba8ec6ed833d6f872e2c2
CAT5NEKO <JFKnet1984@gmail.com>
2024-02-02 17:47:06 +0900

    modify

--- netlify/functions/nyagone/nyagone.js
+++ netlify/functions/nyagone/nyagone.js
@@ -1,17 +1,39 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
-
-module.exports = { handler }
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 410,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Nyagone</title>
+                <style>
+                    body {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        font-family: "Arial Black";
+                        height: 100vh;
+                        margin: 0;
+                        background-color: #141438;
+                    }
+                    h1 {
+                        text-align: center;
+                        font-size: 20em;
+                        font-family: "Arial Black";
+                        color: #e3dfdf;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>410</h1>
+                <h2>THIS PAGE IS GONE</h2>
+            </body>
+            </html>
+        `,
+  };
+};
