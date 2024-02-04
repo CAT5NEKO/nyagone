198cf4b5fa5c84acc43a7d716be1802bc115859c
CAT5NEKO <JFKnet1984@gmail.com>
2024-02-04 23:31:55 +0900

    modify

--- nyagone/pages/index.js
+++ nyagone/pages/index.js
@@ -1,23 +1,20 @@
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 const Home = () => {
-  const router = useRouter();
+    useEffect(() => {
+        fetch('/.netlify/functions/status')
+            .then((response) => {
+                if (!response.ok) {
+                    console.error('Failed to fetch status:', response.status);
+                }
+            });
+    }, []);
 
-  useEffect(() => {
-    fetch('/.netlify/functions/status')
-        .then((response) => {
-          if (!response.ok) {
-            router.push('/error');
-          }
-        });
-  }, []);
-
-  return (
-      <div>
-        <h1>THIS SITE IS GONE!</h1>
-      </div>
-  );
+    return (
+        <div>
+            <h1>GONE!</h1>
+        </div>
+    );
 };
 
 export default Home;
