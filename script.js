4a551e6a51e0ab8d4620de53dcee3ab49c03b8e2
CAT5NEKO <JFKnet1984@gmail.com>
2024-02-05 15:14:28 +0900

    modify

--- script.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const colors = ['#1a237e', '#1e304d', '#0d243d', '#0a1f31', '#1e88e5'];
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const randomColor = colors[randomIndex];
-
-    // Set background color
-    document.querySelector('.gone-container').style.backgroundColor = randomColor;
-
-})();
